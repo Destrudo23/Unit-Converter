9c89c56cc4bb0fd9cc0fde88eb436d1ae55c6478
Destrudo23 <51265520+Destrudo23@users.noreply.github.com>
2023-02-22 16:59:49 +0100

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+const renderContainer = document.getElementById("render-container");
 
+
+convertBtn.addEventListener("click", function() {
+  let calculationResult = ""
+  calculationResult = `
+    <div id="render-div">
+      <h3>Length (Meter/Feet)</h3><br>
+      ${JSON.parse(inputEl.value).toFixed(3)} meters = ${JSON.parse(inputEl.value) * 3.281} feet | ${JSON.parse(inputEl.value)} feet = ${JSON.parse(inputEl.value) / 3.281} meters
+    </div>
+    <div id="render-div">
+      <h3>Volume (Liters/Gallons)</h3><br>
+      ${JSON.parse(inputEl.value).toFixed(3)} liters = ${JSON.parse((inputEl.value) * 0.264).toFixed(3)} gallons | ${JSON.parse(inputEl.value).toFixed(3)} gallons = ${JSON.parse((inputEl.value) / 0.264).toFixed(3)} liters
+    </div>
+    <div id="render-div">
+      <h3>Mass (Kilogramms/Pounds)</h3><br>
+      ${JSON.parse(inputEl.value).toFixed(3)} kilogramms = ${JSON.parse((inputEl.value) * 2.204).toFixed(3)} pounds | ${JSON.parse(inputEl.value).toFixed(3)} pounds = ${JSON.parse((inputEl.value) / 2.204).toFixed(3)} kilogramms
+    </div>
+    `
+  renderContainer.innerHTML = calculationResult
+  }
+)
